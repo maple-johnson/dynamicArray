224bb8fa55c681e02b99714609749b4a7fabcfd5
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-04-08 11:51:28 -0700

    Added placement for get, and set places for other methods.

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,3 +1,23 @@
 public class DynamicStringList implements StringList {
+
+    // Get
+    public String get(int index)
+    {
+        return "";
+    }
+
+    // Set
+
+    
+    // Add
+
+
+    // Remove
+
+
+    // Size
+
+
+    // Capacity
     
 }
