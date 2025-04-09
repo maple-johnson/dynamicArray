ddc5c0bd06ee4ab6f1c1dc9cae5af0835e80e0ef
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-04-09 14:55:19 -0700

    Added a test for get checking retrieval for an available index.

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,8 +1,22 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class DynamicStringListTest 
 {
 
     // ******************** Testing for Get ********************
-
+    @Test
+    void testGet_availableIndex()
+    {
+        // Arrange
+        String[] inputArray = {"Bulbasaur", "Ivysaur", "Venusaur"};
+        DynamicStringList arr = new DynamicStringList(inputArray);
+        int input = 1;
+        // Act
+        String actual = arr.get(input);
+        // Assert
+        assertEquals("Ivysaur", actual);
+    }
 
     // ******************** Testing for Set ********************
 
